397ca3d77b64ff32c462c357b4f0c7164ffeb2f4
Conchy <xConchyx@gmail.com>
2024-07-30 12:27:51 +0200

    first commit

---
+++ src/test/java/org/factzoopia/zootopia/ZootopiaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.factzoopia.zootopia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZootopiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
