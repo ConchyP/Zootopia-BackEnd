6a83d32f6c1ce5b3ee1b6d167e54ad4040115cf2
ArturoAlvarezY <arturoavilamarketing@gmail.com>
2024-08-05 16:48:49 +0200

    Creating the diferent kind of application properties and making the models

---
+++ src/main/java/org/factzoopia/zootopia/models/Animals.java
@@ -0,0 +1,12 @@
+package org.factzoopia.zootopia.models;
+
+public class Animals{
+
+    private enum Felines {Lyon, Tiger, Leopard};
+    private enum Canines {Foxes, Wolves, Jackales};
+    private enum Reptiles {Crocodiles, Snakes, Iguanas};
+    private enum Mustelides {Nutria, Weasels, Badgers};
+    private enum Leporidae {Rabbits, Hares};
+
+    
+}
