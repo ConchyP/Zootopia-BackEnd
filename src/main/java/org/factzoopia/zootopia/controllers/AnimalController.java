0da3f024332eb69dfb3b3e966acd7c2451921863
ArturoAlvarezY <arturoavilamarketing@gmail.com>
2024-08-09 12:06:33 +0200

    fixing imports

--- src/main/java/org/factzoopia/zootopia/controllers/AnimalController.java
+++ src/main/java/org/factzoopia/zootopia/controllers/AnimalController.java
@@ -1,11 +1,11 @@
 package org.factzoopia.zootopia.controllers;
 
+import java.util.List;
+
 import org.factzoopia.zootopia.models.Animal;
 import org.factzoopia.zootopia.services.AnimalService;
-import org.hibernate.mapping.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -15,7 +15,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.service.annotation.PutExchange;
 
 @RestController
 @RequestMapping(path = "${api-endpoint}/animals")
