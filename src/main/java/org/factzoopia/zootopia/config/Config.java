e91a00d84d08bae5f73e0ca2782dc7eb36f1efe6
ArturoAlvarezY <arturoavilamarketing@gmail.com>
2024-08-12 15:52:37 +0200

    fixing h2 trouble

--- src/main/java/org/factzoopia/zootopia/config/Config.java
+++ src/main/java/org/factzoopia/zootopia/config/Config.java
@@ -5,10 +5,13 @@ import java.util.Arrays;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -30,14 +33,19 @@ public class Config {
             .formLogin(form -> form.disable())
             .logout(out -> out
                 .logoutUrl(endpoint + "/logout")
-                .deleteCookies("Cookie_1")
+                .deleteCookies("JSESSIONID")
                 )
-            .authorizeHttpRequests(auth -> auth
-            .requestMatchers("api/v1/animals").permitAll()
-                .anyRequest().authenticated())
-            .httpBasic(Customizer.withDefaults());
+          .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(AntPathRequestMatcher.antMatcher("/h2-console/**")).permitAll()
+                        .requestMatchers(HttpMethod.GET, endpoint + "/countries").hasAnyRole("USER","ADMIN")
+                        .requestMatchers(HttpMethod.POST, endpoint + "/countries").hasRole("ADMIN")
+                        .anyRequest().authenticated())
+                .httpBasic(Customizer.withDefaults())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED));
 
-            return http.build();
+        http.headers(header -> header.frameOptions(frame -> frame.sameOrigin()));
+
+        return http.build();
     }
     
    @Bean
